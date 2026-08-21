74ed49a37e6f932376879f5ed0df095139a0426a
monushah108 <sonuk627422@gmail.com>
2026-08-21 23:28:05 +0530

    fix:users model path

--- folderDriver client/src/admin/pages/Users.jsx
+++ folderDriver client/src/admin/pages/Users.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CanAccess from "../components/CanAccess.jsx";
-import Modal from "../components/Modle.jsx";
+import Modal from "../../components/models/Modle.jsx";
 
 import {
   useGetUsersQuery,
