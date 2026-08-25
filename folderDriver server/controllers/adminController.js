7fce4b2af174e191c8f508b2cbf2cd1a2adea499
monushah108 <sonuk627422@gmail.com>
2026-08-25 07:38:00 +0530

    fix admin controller

--- folderDriver server/controllers/adminController.js
+++ folderDriver server/controllers/adminController.js
@@ -379,8 +379,8 @@ export const registerAdmin = async (req, res, next) => {
 
     res.cookie("admin_access_token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
